1d36fca5bd17c44d209221aad49c7c53a9ba3aab
utopszkij <tibor.fogler@gmail.com>
2017-01-15 16:30:34 +0100

    unittest

---
+++ UnitTest/admin/test.php
@@ -0,0 +1,25 @@
+<?php
+require_once "tests/testJoomlaFramework.php";
+
+class adaloginTest extends PHPUnit_Framework_TestCase {
+	public $ada;
+	
+	function __construct() {
+		global $testData,$componentName,$viewName;
+		define('JPATH_COMPONENT', 'pvoks/site');
+		$componentName = 'pvoks';
+		$viewName = 'pvoks';
+		parent::__construct();
+	}
+    protected function setUp() {
+		global $testData,$componentName;
+		$testData->clear();
+		$componentName = 'Adalogin';
+		$this->setupTestDataForCorrectCall();
+		$this->ada = new AdaloginModelAda_obj($testData);
+	}
+    public function test_0()  {
+		global $testData;
+    }
+}	
+?>
